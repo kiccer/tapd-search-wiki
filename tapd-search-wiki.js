4d79b49ade9269fa1fb89de20d1711ef17b725cf
kiccer <1072907338@qq.com>
2020-12-21 11:30:20 +0800

    [update] if 改为 show，节省开销

--- tapd-search-wiki.js
+++ tapd-search-wiki.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【tapd】一键查询所有项目中的wiki
 // @namespace    https://github.com/kiccer/tapd-search-wiki
-// @version      1.2.0
+// @version      1.2.1
 // @description  为了方便在tapd的wiki中查找接口而开发
 // @author       kiccer<1072907338@qq.com>
 // @copyright    2020, kiccer (https://github.com/kiccer)
@@ -277,7 +277,7 @@
                         <transition name="fade">
                             <div
                                 class="back-top"
-                                v-if="toggle.showBackTop"
+                                v-show="toggle.showBackTop"
                                 @click="backTop"
                             >
                                 <i class="el-icon-arrow-up" />
