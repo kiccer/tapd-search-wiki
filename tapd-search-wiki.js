6f2ba452f5ea83038081dba2e9f2560de3694130
kiccer <1072907338@qq.com>
2021-01-12 14:24:06 +0800

    [update] 优化滚动预测

--- tapd-search-wiki.js
+++ tapd-search-wiki.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【tapd】一键查询所有项目中的wiki
 // @namespace    https://github.com/kiccer/tapd-search-wiki
-// @version      3.4.0
+// @version      3.4.1
 // @description  为了方便在tapd的wiki中查找接口而开发
 // @author       kiccer<1072907338@qq.com>
 // @copyright    2020, kiccer (https://github.com/kiccer)
@@ -558,7 +558,10 @@
                             const frameInfo = this.previewFrames.find(n => n.url === url)
                             if (frameInfo) {
                                 this.$set(frameInfo, 'wd', this.wd)
-                                this.rollingForecast(frameInfo)
+                                // 滚动预测
+                                setTimeout(() => {
+                                    this.rollingForecast(frameInfo)
+                                })
                             }
                         }
                     },
