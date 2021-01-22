cd1d43fcfa1480824b67158d68334c0a313e173b
kiccer <1072907338@qq.com>
2021-01-22 10:41:51 +0800

    [update] 增加延迟

--- tapd-search-wiki.js
+++ tapd-search-wiki.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         【tapd】一键查询所有项目中的wiki
 // @namespace    https://github.com/kiccer/tapd-search-wiki
-// @version      3.6.0
+// @version      3.6.1
 // @description  为了方便在tapd的wiki中查找接口而开发
 // @author       kiccer<1072907338@qq.com>
 // @copyright    2020, kiccer (https://github.com/kiccer)
@@ -748,7 +748,7 @@
                                     })
                                 }
                             })
-                        })
+                        }, 1000)
                     },
 
                     // 在增加预览页面弹窗关闭之后
